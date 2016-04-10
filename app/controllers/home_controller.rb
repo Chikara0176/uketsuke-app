ddc4ef1ccd422f17cedf38fa21a3a10a9260bd2d
Chikara0176 <chikara.1993@gmail.com>
2016-04-10 10:50:06 +0900

    GETリクエスト追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,17 @@
 class HomeController < ApplicationController
+  require 'net/http'
+  require 'uri'
+  require 'pp'
+  # require 'httpclient'
+  require 'open-uri'
   def index
-
+    # @uri = URI.parse("https://api.chatwork.com/v1")
+    open("https://api.chatwork.com/v1/my/status",
+         "X-ChatWorkToken" => "8451e9de6e295633c6144e7ed8a2f8ae"
+        ){|f|
+          f.each_line{|line| p line}
+          # binding.pry
+        }
   end
 
   def show
