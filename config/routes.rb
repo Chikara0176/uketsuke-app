23181b35ae4795ccbc9ab2dab599cc52bde759f3
Ryusuke Hiramitsu <rh.a28.yku@gmail.com>
2016-04-10 16:21:47 +0900

    ページ修正とpostの手前まで

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'home#top'
 
-  get '/show/' => 'home#show'
+  get '/index' => 'home#index'
   get '/member' => 'home#member'
+  get '/thanks' => 'home#thanks'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
