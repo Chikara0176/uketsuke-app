116028b54acfb572a81848bb7424ea870837d4e4
Chikara0176 <chikara.1993@gmail.com>
2016-04-10 13:31:50 +0900

    json読み取り

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,21 @@ class HomeController < ApplicationController
   require 'pp'
   # require 'httpclient'
   require 'open-uri'
+  require 'json'
   def index
+    json_file_path = 'app/assets/shikaku_contact.json'
+    # 読み込んで
+    @json_data = open(json_file_path) do |io|
+      JSON.load(io)
+    end
+
     # @uri = URI.parse("https://api.chatwork.com/v1")
-    open("https://api.chatwork.com/v1/my/status",
-         "X-ChatWorkToken" => "8451e9de6e295633c6144e7ed8a2f8ae"
-        ){|f|
-          f.each_line{|line| p line}
-          # binding.pry
-        }
+    # open("https://api.chatwork.com/v1/my/status",
+    #      "X-ChatWorkToken" => "8451e9de6e295633c6144e7ed8a2f8ae"
+    #     ){|f|
+    #       f.each_line{|line| p line}
+    #       # binding.pry
+    #     }
   end
 
   def show
