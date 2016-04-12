9000a342cc9d36e49b70f0bb1eff47bc2aed8cf9
Chikara0176 <chikara.1993@gmail.com>
2016-04-12 09:44:44 +0900

    APIトークン入れ替え

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,7 @@ class HomeController < ApplicationController
   end
 
   def index
+    # 資格スクエアALL窓のメンバー情報
     json_file_path = 'app/assets/shikaku_contact.json'
     # 読み込んで
     @all_member = open(json_file_path) do |m|
@@ -23,9 +24,14 @@ class HomeController < ApplicationController
 
   def thanks
     @c = HTTPClient.new()
+    # 平光さんの個人ID
     @num = 1486652
-    @header = {"X-ChatWorkToken" => "8451e9de6e295633c6144e7ed8a2f8ae"}
-    @res = @c.post('https://api.chatwork.com/v1/rooms/39085887/messages',{ "body" => "[To:#{@num}]来客です。"}, @header )
+    # 平光さん個人の部屋ID
+    @room_num = 39085887
+    # 資格スクエアお知らせトークン
+    @header = {"X-ChatWorkToken" => "0fc7920ba8eb9214d877b21854e82dc7"}
+
+    @res = @c.post('https://api.chatwork.com/v1/rooms/#{39085887}/messages',{ "body" => "[To:#{@num}]来客です。"}, @header )
     p @res
   end
 
